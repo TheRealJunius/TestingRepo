ac63abac77f213cfcf42207c6fafab5606436802
Gurau Nikolas <ariciultepos2003@yahoo.com>
2018-08-29 17:57:42 +0300

    Changed the grid and the graphics resolution

--- Engine/Grid.h
+++ Engine/Grid.h
@@ -9,10 +9,10 @@ class Grid
 private:
 	Graphics& gfx;
 public:
-	static constexpr int Width = 32; //16
-	static constexpr int Height = 16; //12
-	static constexpr int CellDimensions = 32; //50
-	static constexpr int Spacing = 2; //0
+	static constexpr int Width = 64; //32
+	static constexpr int Height = 32; //16
+	static constexpr int CellDimensions = 16; //32
+	static constexpr int Spacing = 0; //2
 
 	Grid(Graphics& gfx);
 	void DrawCell(Vec2 loc, Color c);
